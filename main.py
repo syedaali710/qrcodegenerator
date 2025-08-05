9217fa7c9d6b393625ce1029226703d9273a4ca5
syedaali710 <alisyeda710@gmail.com>
2025-08-05 13:52:54 +0400

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import qrcode as a qr
+import qrcode as a qr #download this file first
 img = qr.make("hello") #paste any text or link that u need to generate a qrcode
 img.save("sammy_youtube.png") #save ur qr code as image
 
